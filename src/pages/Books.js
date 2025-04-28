eab08402713407dbd51122e7bef8380d31334c7d
Islam Aref <125500794+islam-aref@users.noreply.github.com>
2025-04-29 01:30:35 +0300

    Update Books.js

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -4,7 +4,43 @@ import Footer from "../components/Footer/Footer";
 import styles from "../styles/Books.module.css";
 
 const books = [
-  // ... (keep your existing books array)
+
+  {
+    id: 1,
+    title: "صياغة استراتيجيات المستقبل التكنولوجي والعلمي",
+    author: "د. فريد النجار",
+    image: "/Assets/Images/book1.png",
+  },
+  {
+    id: 2,
+    title: "الإدارة التكنولوجية للموارد البشرية بنظرية الوفرة",
+    author: "د. فريد النجار",
+    image: "/Assets/Images/book2.png",
+  },
+  {
+    id: 3,
+    title: "الثقافة السلوكية للتواصل مع الطفل",
+    author: "د. فريد النجار",
+    image: "/Assets/Images/book3.png",
+  },
+  {
+    id: 4,
+    title: "تكنولوجيا التسويق",
+    author: "د. فريد النجار",
+    image: "/Assets/Images/book4.png",
+  },
+  {
+    id: 5,
+    title: "أبناء الطلاق",
+    author: "د. مروة عماد",
+    image: "/Assets/Images/book5.png",
+  },
+  {
+    id: 6,
+    title: "مدخل إلى الاستشارة الفلسفية",
+    author: "د. مصطفى النشار",
+    image: "/Assets/Images/book6.png",
+  },
 ];
 
 const Books = () => {
